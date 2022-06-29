2020f57a7d99d6de8ddbbacc8b7026e71df2fa43
Diego <diegoarlechuga@gmail.com>
2022-06-29 08:53:09 -0500

    disableBaseline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react';
-import {Button} from '@nextui-org/react'
+import { Navbar } from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider disableBaseline>
+      <Navbar/>
       <Component {...pageProps} />  
     </NextUIProvider>
   )
