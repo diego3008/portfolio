f38d4f9ed11f893c24c36967e63cdcb6bc5229c1
Diego <diegoarlechuga@gmail.com>
2022-06-29 12:36:20 -0500

    Added animate.css and created header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react';
 import { Navbar } from '../components/Navbar';
+import 'animate.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+ 
+
   return (
     <NextUIProvider disableBaseline>
       <Navbar/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import { PageLayout } from '../components';
+import { Header } from '../components/Header';
 
 
 const Home: NextPage = () => {
   return (
-		<PageLayout title="Diego Rubalcava - Home">
+		<PageLayout title="Diego Rubalcava">
 			<main className="main-container relative min-h-[100vh] pl-[0.45rem] pr-[0.45rem] sm:pl-0 sm:pr-0">
+				<Header/>
 			</main>
 		</PageLayout>
 	);
