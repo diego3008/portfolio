da6e4207584db21967d82a1b7d78c65eeecff6b1
Diego <diegoarlechuga@gmail.com>
2022-06-29 08:53:44 -0500

    changed page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,8 @@ import { PageLayout } from '../components';
 
 const Home: NextPage = () => {
   return (
-		<PageLayout title="Diego Rubalcava">
+		<PageLayout title="Diego Rubalcava - Home">
 			<main className="main-container relative min-h-[100vh] pl-[0.45rem] pr-[0.45rem] sm:pl-0 sm:pr-0">
-        
 			</main>
 		</PageLayout>
 	);
